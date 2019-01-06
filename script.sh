b6bd77342402d99bbc428d8e1b6122fffc481c9e
Xuwen <xuwen@ion.one>
2019-01-06 18:39:07 +0800

    friendbot url change

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sleep 40
 
-/home/ubuntu/go/bin/horizon serve --ingest --db-url postgresql://horizonuser:password@localhost/horizondb --stellar-core-url http://127.0.0.1:8080 --stellar-core-db-url postgresql://postgres:password@localhost/stellar --friendbot-url http://localhost:8004/
+/home/ubuntu/go/bin/horizon serve --ingest --db-url postgresql://horizonuser:password@localhost/horizondb --stellar-core-url http://127.0.0.1:8080 --stellar-core-db-url postgresql://postgres:password@localhost/stellar --friendbot-url https://friendbot.ion.one/
